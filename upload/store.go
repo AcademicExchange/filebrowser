ce2260dbb12c0da0621a63d3a1af84ef031113b4
YoungerLee <651238355@qq.com>
2020-09-27 09:56:01 +0800

    feat: provide command line client

--- upload/store.go
+++ upload/store.go
@@ -1,79 +1,79 @@
 package main
 
 import (
-	"encoding/gob"
-	"io"
-	"os"
-	"sync"
+    "encoding/gob"
+    "io"
+    "os"
+    "sync"
 )
 
 type Store struct {
-	data record
-	mu   sync.RWMutex
-	file *os.File
+    data record
+    mu   sync.RWMutex
+    file *os.File
 }
 
 type record struct {
-	UUID      string
-	Jwt       string
+    UUID string
+    Jwt  string
 }
 
 func NewStore(filename string) *Store {
-	s := &Store{data: record{}}
-	f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		log.Fatalf("open store failed:", err)
-	}
-	s.file = f
-	if err := s.load(); err != nil {
-		log.Fatalf("load store failed:", err)
-	}
-	os.Truncate(filename, 0)
-	return s
+    s := &Store{data: record{}}
+    f, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+    if err != nil {
+        log.Fatalf("open store failed:", err)
+    }
+    s.file = f
+    if err := s.load(); err != nil {
+        log.Fatalf("load store failed:", err)
+    }
+    os.Truncate(filename, 0)
+    return s
 }
 
 func (s *Store) GetUUID() string {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	return s.data.UUID
+    s.mu.RLock()
+    defer s.mu.RUnlock()
+    return s.data.UUID
 }
 
 func (s *Store) SetUUID(uuid string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.data.UUID = uuid
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.data.UUID = uuid
 }
 
 func (s *Store) GetJWT() string {
-	s.mu.RLock()
-	defer s.mu.RUnlock()
-	return s.data.Jwt
+    s.mu.RLock()
+    defer s.mu.RUnlock()
+    return s.data.Jwt
 }
 
 func (s *Store) SetJWT(jwt string) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
-	s.data.Jwt = jwt
+    s.mu.Lock()
+    defer s.mu.Unlock()
+    s.data.Jwt = jwt
 }
 
 func (s *Store) load() error {
-	if _, err := s.file.Seek(0, 0); err != nil {
-		return err
-	}
-	d := gob.NewDecoder(s.file)
-	var err error
-	var r record
-	if err = d.Decode(&r); err == nil {
-		s.data.UUID = r.UUID
-		s.data.Jwt = r.Jwt
-	}
-	if err == io.EOF {
-		return nil
-	}
-	return err
+    if _, err := s.file.Seek(0, 0); err != nil {
+        return err
+    }
+    d := gob.NewDecoder(s.file)
+    var err error
+    var r record
+    if err = d.Decode(&r); err == nil {
+        s.data.UUID = r.UUID
+        s.data.Jwt = r.Jwt
+    }
+    if err == io.EOF {
+        return nil
+    }
+    return err
 }
 
 func (s *Store) Save() error {
-	e := gob.NewEncoder(s.file)
-	return e.Encode(s.data)
+    e := gob.NewEncoder(s.file)
+    return e.Encode(s.data)
 }
